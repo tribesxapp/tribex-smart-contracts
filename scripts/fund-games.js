c392a30b5453518798b26a43baf0d0771818f5eb
gabesan21 <141934803+suportelider@users.noreply.github.com>
2024-01-18 19:23:00 -0300

    new contracts and events

--- scripts/fund-games.js
+++ scripts/fund-games.js
@@ -27,11 +27,3 @@ module.exports = async (deployer, network, accounts) => {
   }
   exit();
 };
-
-
-// base:0xFe4559392aF0E6988F2d7A4E6447a2E702Ff215d
-// Multisig na Base
-// Carteira admin
-
-// Taxas
-// base:0x2c1cC7CD99993EC95f9a4F6eF76f8eDccee529CD
